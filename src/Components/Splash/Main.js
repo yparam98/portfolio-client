648afabb61c6ecd8a296ab9256792aab9002b3e6
yparam98 <yathavanparamesh1998@gmail.com>
2020-11-09 01:06:27 -0500

    initialized main page

--- src/Components/Splash/Main.js
+++ src/Components/Splash/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import '../../assets/css/Splash.css';
 
 class Main extends Component {
     constructor(props) {
@@ -8,9 +9,9 @@ class Main extends Component {
 
     render() {
         return (
-            <div>
-                <p>Menu</p>
-                <Link to="/portfolio">Portfolio</Link>
+            <div class="site-center stack">
+                <div class="name-title">Yathavan Parameshwaran</div>                
+                <button type="button" class="btn btn-outline-primary"><Link to="/portfolio">Portfolio</Link></button>
             </div>
         )
     }
